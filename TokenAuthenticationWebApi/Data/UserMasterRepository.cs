c04014adbfca3404e31f23a323834d8a3bbee0e9
Eranda <selfnba@gmail.com>
2020-07-15 04:20:56 +0530

    edited

---
+++ TokenAuthenticationWebApi/Data/UserMasterRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TokenAuthenticationWebApi.Models;
+
+namespace TokenAuthenticationWebApi.Data
+{
+    public class UserMasterRepository
+    {
+        //validate user cred
+        public UserMaster ValidateUser(string username, string password)
+        {
+
+            if (username == "user" && password == "password")
+            {
+                UserMaster obj = new UserMaster
+                {
+                    UserName = username,
+                    Password = password
+                };
+
+                return obj;
+            }
+
+            return null;
+
+        }
+
+    }
+}
---
+++ TokenAuthenticationWebApi/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+//using TokenAuthenticationInWebAPI.Models;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+using TokenAuthenticationWebApi.Data;
+using TokenAuthenticationWebApi;
+
+[assembly: OwinStartup(typeof(TokenAuthenticationInWebAPI.App_Start.Startup))]
+
+namespace TokenAuthenticationInWebAPI.App_Start
+{
+    //OAuth Authorization Server.
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Enable CORS 
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+
+                //generating the Toekn
+                TokenEndpointPath = new PathString("/token"),
+
+                //Setting the Token Expired Time (24 hours)
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+
+                //MyAuthorizationServerProvider class will validate the user credentials this is hardcoded :p 
+                Provider = new MyAuthorizationServerProvider()
+            };
+
+            //Token Generations
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
